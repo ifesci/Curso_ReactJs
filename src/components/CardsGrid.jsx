6782105557224df912034e601d8a722a2df93df7
Ricardo Maroquio <maroquio@gmail.com>
2025-04-29 09:46:31 -0300

    implementados passos 6 e 7

--- src/components/CardsGrid.jsx
+++ src/components/CardsGrid.jsx
@@ -6,13 +6,14 @@ const CardsGrid = ({ title, items, cols = 4, onAddToCart }) => {
     <section className="mb-4">
       <h2>{title}</h2>
       <hr />
-      <div className={`row ${colClass} g-3`}>
+      <div className={`row ${colClass} g-3 align-items-stretch`}>
         {items.map((item) => (
           <Card
             key={item.id}
             image={item.image_url}
             title={item.title}
             description={item.description}
+            price={item.price}
             link={`/item/${item.id}`}
             onAddToCartClick={() => onAddToCart(item)}
           />
